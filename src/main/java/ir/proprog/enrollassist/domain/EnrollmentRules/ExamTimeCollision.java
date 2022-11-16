3381835a943dda95db778ae51fef15736aabfced
mohadese.rafiei <mohadese.rafiei@tapsi.cab>
2022-11-16 21:34:19 +0330

    Refactor ExamTimeCollision message

--- src/main/java/ir/proprog/enrollassist/domain/EnrollmentRules/ExamTimeCollision.java
+++ src/main/java/ir/proprog/enrollassist/domain/EnrollmentRules/ExamTimeCollision.java
@@ -15,7 +15,7 @@ public class ExamTimeCollision extends EnrollmentRuleViolation {
 
     @Override
     public String toString() {
-        return String.format("%s is not passed as a prerequisite of %s", section1, section2);
+        return String.format("%s course and %s course has examTime conflict", section1, section2);
     }
 
     @Override
